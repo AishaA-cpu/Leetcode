a4a6cb1b8f2d922851c062c1ae919a126a43dd0d
Aisha <63489725+AishaA-cpu@users.noreply.github.com>
2025-06-03 20:43:12 +0100

    [21] [Time Beats: 100.00%] [Memory Beats: 61.17%] - LeetPush

--- Merge-Two-Sorted-Lists.py
+++ Merge-Two-Sorted-Lists.py
@@ -15,35 +15,30 @@ class Solution:
         if not list2:
             res = list1
             return res
+        
+        curr, temp, res = None, None, None
+        
         if list1.val <= list2.val:
             curr = list1
-            res = curr
             temp = list2
-            while curr.next and temp:
-                if curr.next.val <= temp.val:
-                    curr = curr.next
-                else:
-                    temp2 = curr.next
-                    curr.next = temp
-                    temp = temp2
-                    curr = curr.next
-            if temp is not None:
-                curr.next = temp
         else:
             curr = list2
-            res = curr
             temp = list1
-            while curr.next and temp:
-                if curr.next.val <= temp.val:
-                    curr = curr.next
-                else:
-                    temp2 = curr.next
-                    curr.next = temp
-                    temp = temp2
-                    curr = curr.next
-            if temp is not None:
-                curr.next = temp 
-
+        res = curr
+        while curr.next and temp:
+            if curr.next.val <= temp.val:
+                curr = curr.next
+            else:
+                temp2 = curr.next
+                curr.next = temp
+                temp = temp2
+                curr = curr.next
+        if temp is not None:
+            curr.next = temp
+        
         return res
+
+
+
 
          
