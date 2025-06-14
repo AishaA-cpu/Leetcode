b9af62f146b55864ae7d446ca33e3668beb8c6d5
Aisha <63489725+AishaA-cpu@users.noreply.github.com>
2025-06-14 17:19:29 +0100

    [1] [Time Beats: 100.00%] [Memory Beats: 34.00%] - LeetPush

--- Two-Sum.py
+++ Two-Sum.py
@@ -1,8 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         seen = {}
-        for i in range(len(nums)):
-            counterpart = target - nums[i]
-            if counterpart in seen:
-                return [i, seen[counterpart]]
-            seen[nums[i]] = i
+        for i in enumerate(nums):
+            idx, val = i
+            diff = target - val
+            if diff in seen:
+                return [idx, seen[diff]]
+            seen[val] = idx
+
+        
