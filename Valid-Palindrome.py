85c4b1dad0cecea30d2d32e0c3a76afdd46ba057
Aisha <63489725+AishaA-cpu@users.noreply.github.com>
2025-06-14 21:01:57 +0100

    [125] [Time Beats: 40.54%] [Memory Beats: 83.29%] - LeetPush

--- Valid-Palindrome.py
+++ Valid-Palindrome.py
@@ -4,11 +4,10 @@ class Solution:
         i, j = 0, len(s) - 1
         while i <= j:
             if s[i].isalnum() and s[j].isalnum():
-                if s[i].lower() == s[j].lower():
-                    i += 1
-                    j -= 1
-                else:
+                if s[i].lower() != s[j].lower():
                     return False
+                i += 1
+                j -= 1
             elif not s[i].isalnum():
                 i+=1
             elif not s[j].isalnum():
